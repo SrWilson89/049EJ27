5d15c840f4b7342aa1a29a2edd0461419967437a
Angel <angel481989@gmail.com>
2021-09-06 16:05:21 +0200

    es

--- js/motor.js
+++ js/motor.js
@@ -99,17 +99,6 @@ function cargaDatos(){
     if (localStorage.getItem('MysTikets')){
         misProductos=JSON.parse(localStorage.getItem('MysTikets'));
         
-        for (var x = 0; x < miArray.length; x++) {
-            misProductos.push(new Producto(
-                misProductos[x].foto,
-                misProductos[x].titulo,
-                misProductos[x].precioA,
-                misProductos[x].precioF,
-                misProductos[x].descuento,
-                misProductos[x].cantidad,
-                misProductos[x].detalle
-            ))
-        }
         pintar();
     }
 }
